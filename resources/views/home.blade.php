7f655570e47b2bbe650b63f1f405e4a0021e8335
Muhozi Emery <muhozie@gmail.com>
2017-06-11 16:30:11 +0200

    Add Home View

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('title') Home - @stop
+@section('content')
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title">
+                Auction | Sell & Buy
+            </div>
+        </div>
+    </div>
+@stop
